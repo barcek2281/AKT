a78d19b89c30afb3174a9c724001f81a1cd06dc5
barcek2281 <sabdpp17@gmail.com>
2025-03-29 16:38:49 +0500

    crud operation added

--- backend/internal/handler/microGreenHandler.go
+++ backend/internal/handler/microGreenHandler.go
@@ -8,6 +8,8 @@ import (
 	"github.com/barcek2281/AKT/backend/internal/store"
 	"github.com/barcek2281/AKT/backend/models"
 	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type MicroGreenHandler struct {
@@ -37,7 +39,7 @@ func (h *MicroGreenHandler) CreateMicroGreen(w http.ResponseWriter, r *http.Requ
 	if err := json.NewDecoder(r.Body).Decode(&mg); err != nil {
 		logrus.Warn("bad json file")
 		http.Error(w, "Unauthorized", http.StatusBadRequest)
-		return 
+		return
 	}
 
 	if err := h.db.MicrogreenRepo.Add(user_id, mg); err != nil {
@@ -46,5 +48,76 @@ func (h *MicroGreenHandler) CreateMicroGreen(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	w.Write([]byte("nice"))
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *MicroGreenHandler) GetMicroGreen(w http.ResponseWriter, r *http.Request) {
+	userID, ok := r.Context().Value("user_id").(string)
+	if !ok {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	microgreens, err := h.db.MicrogreenRepo.GetMicrogreensByUser(userID)
+	if err != nil {
+		log.Warn("cannot get id")
+		http.Error(w, "Failed to fetch Microgreens", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(microgreens)
+}
+
+func (h *MicroGreenHandler) UpdateMicroGreen(w http.ResponseWriter, r *http.Request) {
+	userID, ok := r.Context().Value("user_id").(string)
+	if !ok {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var mg models.Microgreen
+	if err := json.NewDecoder(r.Body).Decode(&mg); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.db.MicrogreenRepo.Update(userID, mg); err != nil {
+		http.Error(w, "Failed to update Microgreen", http.StatusInternalServerError)
+		logrus.Warn("cannot update model: ", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *MicroGreenHandler) DeleteMicroGreen(w http.ResponseWriter, r *http.Request) {
+	userID, ok := r.Context().Value("user_id").(string)
+	if !ok {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var req struct {
+		ID string `json:"id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		logrus.Warn("cannot delte model: ", err)
+		return
+	}
+
+	objectID, err := primitive.ObjectIDFromHex(req.ID)
+	if err != nil {
+		logrus.Warn("cannot make model: ", err)
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.db.MicrogreenRepo.Delete(userID, objectID); err != nil {
+		logrus.Warn("cannot delete model: ", err)
+		http.Error(w, "Failed to delete Microgreen", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
