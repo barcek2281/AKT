48623f83ac2fa81a50cfcfd435e70f949e5cf510
barcek2281 <sabdpp17@gmail.com>
2025-03-29 13:32:02 +0500

    microrepository

--- backend/internal/repository/microGreenRepository.go
+++ backend/internal/repository/microGreenRepository.go
@@ -1,1 +1,57 @@
 package repository
+
+import (
+	"context"
+	"time"
+
+	"github.com/barcek2281/AKT/backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MicroGreenRepository struct {
+	collection *mongo.Collection
+}
+
+func (m *MicroGreenRepository) Add(userID string, microgreen models.Microgreen) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	userObjectID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return err
+	}
+
+	microgreen.ID = primitive.NewObjectID()
+	microgreen.UserID = userObjectID
+
+	_, err = m.collection.InsertOne(ctx, microgreen)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *MicroGreenRepository) GetMicrogreensByUser(userID string) ([]models.Microgreen, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	objectID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.M{"user_id": objectID}
+	cursor, err := r.collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var microgreens []models.Microgreen
+	if err = cursor.All(ctx, &microgreens); err != nil {
+		return nil, err
+	}
+
+	return microgreens, nil
+}
--- backend/models/microgreens.go
+++ backend/models/microgreens.go
@@ -1,11 +1,26 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Microgreen struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	UserID    primitive.ObjectID `bson:"user_id"` // Связь с пользователем
-	Name      string             `bson:"name"`
-	GrowthDay int                `bson:"growth_day"`
-	Quantity  int                `bson:"quantity"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`   // Уникальный идентификатор
+	UserID      primitive.ObjectID `bson:"user_id" json:"user_id"`         // Владелец партии
+	Name        string             `bson:"name"`            // Название (Базилик, Редис и т.д.)
+	SowingDate  time.Time          `bson:"sowing_date"`     // Дата посева
+	Substrate   string             `bson:"substrate"`       // Тип субстрата (кокос, почва и пр.)
+	HarvestDays int                `bson:"harvest_days"`    // Примерное время до сбора (в днях)
+	Notes       string             `bson:"notes,omitempty"` // Доп. заметки
+	GrowthLog   []GrowthEntry      `bson:"growth_log"`      // Записи о росе
+}
+
+// Фенологический журнал (Запись о росте)
+type GrowthEntry struct {
+	Date     time.Time `bson:"date"`      // Дата записи
+	Height   float64   `bson:"height"`    // Высота ростков (в см)
+	Notes    string    `bson:"notes"`     // Комментарии (о поливе, освещении)
+	PhotoURL string    `bson:"photo_url"` // Фото URL
 }
